f7a14f556657b1296d5bcdb4e1df3cbc3cd1556a
zineddinechabbaki <110823304+zineddinechabbaki@users.noreply.github.com>
2023-01-17 12:01:12 +0100

    Ajoutez des fichiers projet.

---
+++ buyTikect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace buyTikect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
